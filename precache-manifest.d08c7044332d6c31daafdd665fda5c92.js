6472e1ae78dafe1f32709e609dde2804fa4c1e37
natasha-robarge <robarge.natasha7@gmail.com>
2019-02-23 16:09:18 -0600

    Updates

---
+++ precache-manifest.d08c7044332d6c31daafdd665fda5c92.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "1446d6ec194445c3572d",
+    "url": "/robofriends/static/js/runtime~main.1446d6ec.js"
+  },
+  {
+    "revision": "c7401caa1f9a019ed7f2",
+    "url": "/robofriends/static/js/main.c7401caa.chunk.js"
+  },
+  {
+    "revision": "29a988b3bec0a44d0aa4",
+    "url": "/robofriends/static/js/1.29a988b3.chunk.js"
+  },
+  {
+    "revision": "c7401caa1f9a019ed7f2",
+    "url": "/robofriends/static/css/main.5e383930.chunk.css"
+  },
+  {
+    "revision": "29a988b3bec0a44d0aa4",
+    "url": "/robofriends/static/css/1.e2337dee.chunk.css"
+  },
+  {
+    "revision": "ad6c90217c57204dfb14422ccc1bb43a",
+    "url": "/robofriends/index.html"
+  }
+];
